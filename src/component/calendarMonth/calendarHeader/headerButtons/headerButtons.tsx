3e9a80735464cf53fb41ffa959ed201f89e371ba
AnastasijaKiyan <anastasijakiyan@gmail.com>
2018-12-29 00:51:09 +0200

    commit4

--- src/component/calendarMonth/calendarHeader/headerButtons/headerButtons.tsx
+++ src/component/calendarMonth/calendarHeader/headerButtons/headerButtons.tsx
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 
-class HeaderButtons extends Component {
-  constructor(props: any) {
+interface Button {
+  id: number;
+  name: string
+}
+
+type Props = { };
+type State = { buttonSelectedId: number };
+
+class HeaderButtons extends Component <Props, State> {
+
+  private buttons: Button[] = [];
+
+  constructor(props: Props) {
     super(props);
+    this.state = {buttonSelectedId: 2};
+  this.buttons.push({ id: 1, name: "Properties" }, { id: 2, name: "Calendar" }, { id: 3, name: "Logs"});
+  }
+
+  press(id: number, e: React.MouseEvent<HTMLInputElement>):void {
+    this.setState({buttonSelectedId: id});
   }
 
   render() {
     return (
       <div className="headerButtons">
-        <input type="button" value="Properties" />
-        <input type="button" value="Calendar" />
-        <input type="button" value="Logs" />
+      {
+        this.buttons.map((item: Button) => <input key={item.id} className={this.state.buttonSelectedId == item.id ? "clicked" : ""}
+         type="button" name={String(item.id)} value={item.name} onClick={e => this.press(item.id, e)}/>)
+      }
       </div>
     );
   }
